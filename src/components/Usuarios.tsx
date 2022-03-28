8e3009ab4e64d2937c78bfedf0e7f16adf7502ea
Juan Esteban <juanes0202@gmail.com>
2022-03-28 12:04:22 -0500

    creando hook personalizado para listar los usuarios

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,45 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
-import { reqResApi } from '../api/reqRes';
-import { ReqResListado, User } from '../interfaces/reqRes';
+import { useUsers } from '../hooks/useUsers';
 
 export const Usuarios = () => {
 
-    const [users, setUsers] = useState<User[]>([]);
-
-    const paginaRef = useRef(1);
-
-    useEffect(() => {
-        loadUsers();
-    }, [])
-
-
-    const renderUser = ({ id, first_name, last_name, avatar, email }: User) => {
-        return (
-            <tr key={id.toString()}>
-                <td>
-                    <img className='img-thumbnail rounded-circle w-25' src={avatar} alt={first_name} />
-                </td>
-                <td>{first_name} {last_name}</td>
-                <td>{email}</td>
-            </tr>
-        );
-    }
-
-    const loadUsers = async () => {
-        const res = await reqResApi.get<ReqResListado>('/users', {
-            params: {
-                page: paginaRef.current,
-            }
-        })
-
-        if (res.data.data.length > 0) {
-            setUsers(res.data.data);
-            paginaRef.current++;
-        } else {
-            alert('Not found more data');
-        }
-    }
-
+    const { users, renderUser, loadUsers } = useUsers();
 
     return (
         <>
@@ -57,7 +20,7 @@ export const Usuarios = () => {
                 </tbody>
             </table>
 
-            <button className="btn btn-primary"  onClick={loadUsers}>Siguiente</button>
+            <button className="btn btn-primary" onClick={loadUsers}>Siguiente</button>
         </>
     )
 }
---
+++ src/hooks/useUsers.tsx
@@ -0,0 +1,49 @@
+import { useState, useRef, useEffect } from 'react';
+import { User, ReqResListado } from '../interfaces/reqRes';
+import { reqResApi } from '../api/reqRes';
+
+export const useUsers = () => {
+
+    const [users, setUsers] = useState<User[]>([]);
+
+    const paginaRef = useRef(1);
+
+    useEffect(() => {
+        loadUsers();
+    }, [])
+
+
+    const renderUser = ({ id, first_name, last_name, avatar, email }: User) => {
+        return (
+            <tr key={id.toString()}>
+                <td>
+                    <img className='img-thumbnail rounded-circle w-25' src={avatar} alt={first_name} />
+                </td>
+                <td>{first_name} {last_name}</td>
+                <td>{email}</td>
+            </tr>
+        );
+    }
+
+    const loadUsers = async () => {
+        const res = await reqResApi.get<ReqResListado>('/users', {
+            params: {
+                page: paginaRef.current,
+            }
+        })
+
+        if (res.data.data.length > 0) {
+            setUsers(res.data.data);
+            paginaRef.current++;
+        } else {
+            alert('Not found more data');
+        }
+    }
+
+
+    return {
+        users,
+        loadUsers,
+        renderUser,
+    }
+}
