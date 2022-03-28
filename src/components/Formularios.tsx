f0b8b97e6193d001aa3b4130d727c364696f6f01
Juan Esteban <juanes0202@gmail.com>
2022-03-28 17:24:13 -0500

    creando hook personalizadopara el hook de formulario

--- src/components/Formularios.tsx
+++ src/components/Formularios.tsx
@@ -1,18 +1,11 @@
-import { useState } from 'react';
+import { useForm } from '../hooks/useForm';
 
 export const Formularios = () => {
 
-    const [formulario, setFormulario] = useState({
+    const { formulario, email, password, onChange } = useForm({
         email: 'test@test.com',
         password: 'password'
     });
-
-    const onchange = (value: string, field: string) => {
-        setFormulario({
-            ...formulario,
-            [field]: value
-        });
-    }
 
     return (
         <>
@@ -21,21 +14,21 @@ export const Formularios = () => {
             <input type="text"
                 className="form-control"
                 placeholder="E-mail"
-                value={formulario.email}
-                onChange={({ target }) => onchange(target.value, 'email')}
+                value={email}
+                onChange={({ target }) => onChange(target.value, 'email')}
             />
 
             <input type="password"
                 className="form-control mt-2"
                 placeholder="Password"
-                value={formulario.password}
-                onChange={({ target }) => onchange(target.value, 'password')}
+                value={password}
+                onChange={({ target }) => onChange(target.value, 'password')}
 
             />
 
             <code>
                 <pre>
-                    { JSON.stringify(formulario, null, 2) }
+                    {JSON.stringify(formulario, null, 2)}
                 </pre>
             </code>
         </>
